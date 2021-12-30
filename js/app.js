269ab047a28c46146e0312391faec1484b98912d
Tolibjon <92380976+tolibjoninagamov@users.noreply.github.com>
2021-12-30 12:21:05 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,38 @@
+// exersice 1
+
+// for (a = 0; a <= 100; a++){
+//   if (a % 2 == 0){
+//     console.log(a);;
+//   }
+// }
+
+
+
+// extersice 2 
+
+
+// for( i = 0; i <= 100; i++)
+//         if(i%3 == 0)
+//         console.log(i);
+
+
+//   for( i = 0; i <= 100; i++)
+        // if(i%5 == 0)
+        // console.log(i);
+
+
+//exercise 3
+
+// let shokir;
+
+// do {
+//   shokir = prompt("enter a number greater than 100?", 100);
+// }
+// while (shokir < 100 && shokir);
+
+
+//exersice 4
+
+  
+
+
